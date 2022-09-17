18d8b161e1e9f7854a28566951518afd484ab933
pachara <47096657+eccentricPACHARA@users.noreply.github.com>
2022-09-18 00:24:54 +0700

    Add files via upload

---
+++ CSGOExternal/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("ECT.Radar")]
+[assembly: AssemblyDescription("csgo 2d radar")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ECT.Radar")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+[assembly: Guid("c301d66d-b695-4b8d-b5b1-bd8783ae9f31")]
+
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
