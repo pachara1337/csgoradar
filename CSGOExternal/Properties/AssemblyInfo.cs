fe4ca3384c40a42763b9af7e6b0b38054db71067
pachara <47096657+eccentricPACHARA@users.noreply.github.com>
2022-09-19 11:51:07 +0700

    Update AssemblyInfo.cs

--- CSGOExternal/Properties/AssemblyInfo.cs
+++ CSGOExternal/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ECT.Radar")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright © EccentricSoft 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
